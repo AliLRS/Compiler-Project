06a3c3d8c44a8762f51bf1e17d25b2e4722fde20
Maryam Paydar <maryampaydar812@gmail.com>
2023-12-14 15:40:43 -0800

    Update Sema.cpp

--- src/Sema.cpp
+++ src/Sema.cpp
@@ -70,6 +70,15 @@ public:
         }
       }
     }
+
+    if (Node.getOperator() == BinaryOp::Operator::Exp ) {
+      Final* f = (Final*)right;
+
+      if (f->getKind() == Final::ValueKind::Ident) {
+        llvm::errs() << "The exponent only allowed to be a constant.\n";
+        HasError = true;
+      }
+    }
   };
 
   // Visit function for Assignment nodes
